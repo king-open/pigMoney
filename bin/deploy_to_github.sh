afb5a7e5778d56e6165a9d40e6973b8b7d5624a0
king-open <kingwang.0624@gmail.com>
2023-10-31 14:24:52 +0800

    添加 build base 添加自动部署到 GitHub 的脚本

---
+++ bin/deploy_to_github.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+rm -rf dist
+pnpm run build
+cd dist
+git init .
+git add .
+git commit -m deploy
+git remote add origin git@github.com:king-open/pigMoney.git
+git push -f origin main:main
+cd -
