4d0ce032a46a6f529e1b747186afca6582e39032
king-open <kingwang.0624@gmail.com>
2023-11-02 16:16:38 +0800

    使用 vite-plugin-mock 在本地创建 mock 服务器

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import {viteMockServe} from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),Unocss()],
-})
+export default defineConfig(({ command })=>({
+  plugins: [
+    react(),
+    Unocss(),
+    viteMockServe()
+  ],
+}))
