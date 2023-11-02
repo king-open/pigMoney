b68de283ed98800f38e7edf5745fa779426d0be2
king-open <kingwang.0624@gmail.com>
2023-11-02 22:55:32 +0800

    完成 Topnav 组件

--- src/components/Topnav.tsx
+++ src/components/Topnav.tsx
@@ -1,5 +1,14 @@
+import {Icon} from './Icon.tsx';
 import React from 'react';
 
-export const Topnav:React.FC = ()=>{
-  return  <div>Topnav</div>
+interface Props{
+  title?:string
+}
+export const Topnav:React.FC<Props> = ({title = 'pigMoney'}) =>{
+  return (
+    <div text-white flex items-center p-16px>
+      <Icon name="menu" className="w-24px h-24px mr-16px" />
+      <h1 text-24px>{title}</h1>
+    </div>
+  )
 }
