1b9f1da9b6979adfa55a9c418cbca19920c126dd
king-open <kingwang.0624@gmail.com>
2023-11-06 22:52:25 +0800

    修复手机上的表单样式bug

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -45,8 +45,9 @@ export const SignInPage: React.FC = () => {
         <div>
           <span j-form-label>验证码 {error.code?.[0] && <span text-red>{error.code[0]}</span>} </span>
           <div flex gap-x-16px>
-            <input j-input-text type="text" placeholder="六位数字" value={data.code} onChange={e => setData({ code: e.target.value })}/>
-            <button j-btn>发送验证码</button>
+            <input j-input-text type="text" placeholder="六位数字" max-w="[calc(40%-80%)]"
+                   value={data.code} onChange={e => setData({ code: e.target.value })}/>
+            <button max-w="[calc(60%-8px)]" shrink-0  j-btn>发送验证码</button>
           </div>
         </div>
         <div mt-100px>
