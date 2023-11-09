63564c62e79b9c6529985548eccd177778f0c539
king-open <kingwang.0624@gmail.com>
2023-11-09 17:14:52 +0800

    验证新建标签表单的正确性

---
+++ src/stores/useCreateTagStore.tsx
@@ -0,0 +1,44 @@
+import create from 'zustand'
+import type { FormError } from '../lib/validate'
+
+type Data = Tag
+
+interface CreateTag {
+  data: Partial<Data>
+  error: FormError<Data>
+  setData: (data: Partial<Data>) => void
+  setError: (error: Partial<FormError<Data>>) => void
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const useCreateTagStore = create<CreateTag>((set, get) => (
+  {
+    data: {
+      kind: 'expenses',
+      sign: '',
+      name: ''
+    },
+    error: {
+      kind: [],
+      sign: [],
+      name: []
+    },
+    setData: (data: Partial<Data>) => {
+      set(state => ({
+        ...state,
+        data: {
+          ...state.data,
+          ...data
+        }
+      }))
+    },
+    setError: (error: Partial<FormError<Data>>) => {
+      set(state => ({
+        ...state,
+        error: {
+          ...error
+        }
+      }))
+    }
+  }
+))
